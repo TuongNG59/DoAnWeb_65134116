42b2336bf82749162568378a5ef2a681b5f0c185
TuongNG59 <nhuynhtuong59@gmail.com>
2026-06-07 20:22:36 +0700

    Tạo và code AdminService

---
+++ DoAnWeb_65134116_NguyenHuynhTuong/src/main/java/com/nguyenhuynhtuong65134116/quancomtam/Services/AdminService.java
@@ -0,0 +1,44 @@
+package com.nguyenhuynhtuong65134116.quancomtam.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nguyenhuynhtuong65134116.quancomtam.Entities.Hoadon;
+import com.nguyenhuynhtuong65134116.quancomtam.Entities.Monan;
+import com.nguyenhuynhtuong65134116.quancomtam.Repositories.HoadonRepository;
+import com.nguyenhuynhtuong65134116.quancomtam.Repositories.MonanRepository;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private HoadonRepository hoadonRepository;
+
+    @Autowired
+    private MonanRepository monanRepository;
+
+    // 1. Lấy tất cả hóa đơn của quán ăn (Mới nhất xếp lên đầu)
+    public List<Hoadon> layTatCaHoaDon() {
+        return hoadonRepository.findAll();
+    }
+
+    // 2. Cập nhật trạng thái đơn hàng (Ví dụ: Chờ xác nhận -> Đang chuẩn bị cơm -> Đã giao)
+    public boolean capNhatTrangThaiDonHang(Integer hoadonId, String trangThaiMoi) {
+        Optional<Hoadon> hoadonOpt = hoadonRepository.findById(hoadonId);
+        if (hoadonOpt.isPresent()) {
+            Hoadon hoadon = hoadonOpt.get();
+            hoadon.setTrangthai(trangThaiMoi);
+            hoadonRepository.save(hoadon);
+            return true;
+        }
+        return false;
+    }
+
+    // 3. Thêm một món ăn mới vào thực đơn quán cơm tấm Anh High
+    public Monan themMonAnMoi(Monan monanMoi) {
+        return monanRepository.save(monanMoi);
+    }
+}
